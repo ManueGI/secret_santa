b92909c80d144a860ef76aa4a69e24af397404d6
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-07-20 00:34:47 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
+  resources :groups do
+    resources :group_members
+    resources :santa_assignements
+  end
 end
