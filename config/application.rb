93496b9d4bcfc44f7a1e43dc6c5ff00eb8de72d6
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-12-08 00:19:43 +0100

    config render

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,15 @@ Bundler.require(*Rails.groups)
 module SecretSanta
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
+
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Configuration for the application, engines, and railties goes here.
     #
