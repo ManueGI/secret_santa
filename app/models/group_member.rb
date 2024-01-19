23b04e7d17165c87872f74b9133b6ece11ec9a7c
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2024-01-19 17:40:02 +0100

    front public_pages

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,6 +1,6 @@
 class GroupMember < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  validates :group, :user, presence: true
+  validates :group, :user, presence: { message: "Ce pseudo n'existe pas" }
   validates :user, uniqueness: { scope: :group, message: 'Cette personne est déja dans le groupe' }
 end
